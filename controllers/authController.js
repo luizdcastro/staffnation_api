cfe3607b62dcea20739ad90b62c82b862e499483
unknown <luizf.dcastro@gmail.com>
2020-10-19 20:40:33 -0300

    user model

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,6 +14,7 @@ exports.register = async (req, res) => {
 		user = await User.create({
 			name: req.body.name,
 			email: req.body.email,
+			phone: req.body.phone,
 			password: req.body.password,
 		});
 
